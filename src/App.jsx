f5269bf4cf1b6c09212b20b2152f3a6106cb1310
Arturo Esqueda <art270112@gmail.com>
2025-04-09 18:04:11 -0600

    Se realizan las carpetas del proyecto con las cuales iniciaremos

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
+import HeroContent from "./Components/HeroContent";
 
-import './App.css'
 
 function App() {
 
 
   return (
-    <>
-    
-        
-    </>
+    <div className="overflow-x-hidden text-gray-900 antialiased selection:bg-cyan-500 selection:text-white">
+      <div className="fixed top-0 -z-10 h-full w-full">
+        <div class="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.13)_0,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)]"></div>
+      </div>
+      <div className="container mx-auto px-8">
+       <HeroContent/>
+      </div>
+    </div>
   )
 }
 
---
+++ src/Components/HeroContent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HeroContent = () => {
+  return (
+    <div>
+      <h1 className='text-3xl'>Luis Rodríguez</h1>
+    </div>
+  );
+}
+
+export default HeroContent;
