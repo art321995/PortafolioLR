fef81403b671c696d37ea6658f13edf1c4f98ef9
Arturo Esqueda <art270112@gmail.com>
2025-04-11 17:36:44 -0600

    Se agregan las imagenes y URL's de los proyectos trabajados

--- src/Constants/index.js
+++ src/Constants/index.js
@@ -62,54 +62,63 @@ export const PROYECTOS = [
         titulo: "Nana Café",
         descripcionP: "Mantenimiento y desarrollo del módulo de compra para la aplicación Nana Café.",
         imagen: "",
-        actividad: "Creación de  modulo en SwiftUI."
+        actividad: "Creación de  modulo en SwiftUI.",
+        url: "https://apps.apple.com/us/app/nana-caf%C3%A9/id6456399529"
     },
     {
         titulo: "BCash",
         descripcionP: "Aplicación para el registro y solicitud de una línea de crédito BCASH.",
         imagen: "",
-        actividad: "Creación y desarrollo de la aplicación en SwiftUI."
+        actividad: "Creación y desarrollo de la aplicación en SwiftUI.",
+        url:"https://play.google.com/store/apps/details?id=com.bafar.b_cash&hl=es_MX&pli=1"
     },
     {
         titulo: "Coppel App",
         descripcionP: "Aplicación móvil de Coppel para compras, pagos y servicios financieros.",
         imagen: "",
-        actividad: "Lanzamiento de la App v1 - v7. Más de 100 lanzamientos a producción. Integraciones y despliegues continuos."
+        actividad: "Lanzamiento de la App v1 - v7. Más de 100 lanzamientos a producción. Integraciones y despliegues continuos.",
+        url: "https://apps.apple.com/mx/app/coppel-compras-en-l%C3%ADnea/id1267382794"
     },
     {
         titulo: "ORDIL",
         descripcionP: "Software de registro de visitas que ofrece un control total de visitantes, de forma eficiente y precisa.",
         imagen: "",
-        actividad: "Creación y desarrollo de la aplicación. Control de ingresos y salidas de visitantes."
+        actividad: "Creación y desarrollo de la aplicación. Control de ingresos y salidas de visitantes.",
+        url:"https://apps.apple.com/mx/app/ordil-id/id1228342544"
     },
     {
         titulo: "IRED",
         descripcionP: "Aplicación que ayuda a encontrar litros completos de gasolina y localizar estaciones confiables en México.",
         imagen: "",
-        actividad: "Mantenimiento y mejoras de la aplicación."
+        actividad: "Mantenimiento y mejoras de la aplicación.",
+        url: ""
     },
     {
         titulo: "ISentinel",
         descripcionP: "Aplicación para el control inteligente y seguro del consumo de GAS LP.",
         imagen: "",
-        actividad: "Mantenimiento y optimización de la aplicación."
+        actividad: "Mantenimiento y optimización de la aplicación.",
+        url: "https://apps.apple.com/us/app/isentinel-gas/id1339190738"
     },
     {
         titulo: "Trivia SAT",
         descripcionP: "Aplicación móvil para jugar trivia y responder preguntas relacionadas con el SAT.",
         imagen: "",
-        actividad: "Creación y desarrollo de la aplicación."
+        actividad: "Creación y desarrollo de la aplicación.",
+        url: ""
     },
     {
         titulo: "ConCrédito Express",
         descripcionP: "Aplicación móvil para el manejo del negocio de las distribuidoras de ConCrédito Express.",
         imagen: "",
-        actividad: "Corrección de errores"
+        actividad: "Corrección de errores",
+        url: ""
     },
     {
         titulo: "Noroeste TV",
         descripcionP: "Aplicación para ver contenido multimedia y radio del Periódico Noroeste.",
         imagen: "",
-        actividad: "Corrección de errores"
+        actividad: "Corrección de errores",
+        url: ""
     }
 ];
