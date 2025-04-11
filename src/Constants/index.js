ba897225a39c02eac99b89b00ec18052208dd98b
Arturo Esqueda <art270112@gmail.com>
2025-04-11 17:07:40 -0600

    Se genera el JS con toda la informacion de texto del Postafolio

--- src/Constants/index.js
+++ src/Constants/index.js
@@ -1,2 +1,115 @@
 
 
+export const HERO_CONTENT = "IOS Developer, con más de 10 años de experiencia en aplicaciones con Swift/SwiftUI";
+
+export const ABOUT_TEXT = "+10 años de experiencia como desarrollador ios. He trabajado en diferentes puestos: Desde desarrollador hasta chapter lead de equipos de más de 25 desarrolladores";
+
+export const EXPERIENCIA = [
+    {
+        empresa:"Softtek",
+        tiempo: "2023 - Present",
+        puesto: "AMS Engennier",
+        descripcion: "Apoyo como desarrollador y chaptear lead de celulas de desarrollo enfocadas a iOS."
+    },
+    {
+        empresa:"Coppel",
+        tiempo: "2023 - 2023",
+        puesto: "Chapter Lead",
+        descripcion: "Chapter lead de un equipo de 30 desarrolladores."
+    },
+    {
+        empresa:"Coppel",
+        tiempo: "2021 - 2023",
+        puesto: "Lead iOS",
+        descripcion: "seguimiento a mas de 10 células de trabajo enfocado a iOS."
+    },
+    {
+        empresa:"Coppel",
+        tiempo: "2019 - 2021",
+        puesto: "Senior iOS",
+        descripcion: "Integraciones de diferentes células, despliegues de desarrollos y seguimiento técnico de nuevas funcionalidades."
+    },
+    {
+        empresa:"Pegasus Control",
+        tiempo: "2017 - 2019",
+        puesto: "Junior iOS",
+        descripcion: "Colaboración en diferentes aplicaciones de la industria de energeticos, asi como diferentes startups internas."
+    },
+    {
+        empresa:"Soluciones Roket",
+        tiempo: "2015 - 2017",
+        puesto: "Junior iOS",
+        descripcion: "Colaboración en diferentes aplicaciones."
+    },
+    {
+        empresa:"Soluciones Roket",
+        tiempo: "2015 - 2016",
+        puesto: "Practicante iOS",
+        descripcion: "Colaboración en la aplicación de DpVale para deportenis."
+    }
+];
+
+export const EDUCACION =[
+    {
+        escuela: "Instituto Tecnológico de Culiacán",
+        año: "2011 - 2016",
+        carrera: "Ingeniería en Tecnologías de la Información y Comunicaciones"
+    }
+]
+
+export const PROYECTOS = [
+    {
+        titulo: "Nana Café",
+        descripcionP: "Mantenimiento y desarrollo del módulo de compra para la aplicación Nana Café.",
+        imagen: "",
+        actividad: "Creación de  modulo en SwiftUI."
+    },
+    {
+        titulo: "BCash",
+        descripcionP: "Aplicación para el registro y solicitud de una línea de crédito BCASH.",
+        imagen: "",
+        actividad: "Creación y desarrollo de la aplicación en SwiftUI."
+    },
+    {
+        titulo: "Coppel App",
+        descripcionP: "Aplicación móvil de Coppel para compras, pagos y servicios financieros.",
+        imagen: "",
+        actividad: "Lanzamiento de la App v1 - v7. Más de 100 lanzamientos a producción. Integraciones y despliegues continuos."
+    },
+    {
+        titulo: "ORDIL",
+        descripcionP: "Software de registro de visitas que ofrece un control total de visitantes, de forma eficiente y precisa.",
+        imagen: "",
+        actividad: "Creación y desarrollo de la aplicación. Control de ingresos y salidas de visitantes."
+    },
+    {
+        titulo: "IRED",
+        descripcionP: "Aplicación que ayuda a encontrar litros completos de gasolina y localizar estaciones confiables en México.",
+        imagen: "",
+        actividad: "Mantenimiento y mejoras de la aplicación."
+    },
+    {
+        titulo: "ISentinel",
+        descripcionP: "Aplicación para el control inteligente y seguro del consumo de GAS LP.",
+        imagen: "",
+        actividad: "Mantenimiento y optimización de la aplicación."
+    },
+    {
+        titulo: "Trivia SAT",
+        descripcionP: "Aplicación móvil para jugar trivia y responder preguntas relacionadas con el SAT.",
+        imagen: "",
+        actividad: "Creación y desarrollo de la aplicación."
+    },
+    {
+        titulo: "ConCrédito Express",
+        descripcionP: "Aplicación móvil para el manejo del negocio de las distribuidoras de ConCrédito Express.",
+        imagen: "",
+        actividad: "Corrección de errores"
+    },
+    {
+        titulo: "Noroeste TV",
+        descripcionP: "Aplicación para ver contenido multimedia y radio del Periódico Noroeste.",
+        imagen: "",
+        actividad: "Corrección de errores"
+    }
+];
