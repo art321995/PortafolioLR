6f08b72582103b801aede125df43000efc0649cb
Arturo Esqueda <art270112@gmail.com>
2025-04-16 16:52:57 -0600

    Se termina el modulo Educacion con animacion

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -1,15 +1,26 @@
 import { GraduationCap } from 'lucide-react';
 import { EDUCACION } from '../Constants';
+import { motion } from "framer-motion";
 
 
 const Education = () => {
  
   return (
     <div className="md:flex w-full mt-10 lg:mt-40 bg-gray-100">
-      <h2 className="flex text-center items-center text-3xl lg:text-5xl font-bold text-blue-500" >Educación</h2>
+      <motion.h2
+        whileInView={{opacity: 1, x: 0}}
+        initial={{opacity: 0, x: -100}}
+        transition={{duration: 2}}
+        className="flex text-center items-center md:p-2 m-4 lg:m-20 text-3xl lg:text-5xl font-bold text-blue-500" >
+          Educación
+      </motion.h2>
       <div className="m-auto flex">
         {EDUCACION.map((edu, index) => (
-          <div key={index} className="p-6">
+          <motion.div key={index} 
+          whileInView={{opacity: 1, x: 0}}
+          initial={{opacity: 0, x: 100}}
+          transition={{duration: 2}}
+            className="p-6">
             <div className="flex items-start gap-4">
               <div className="p-2 bg-primary/10 rounded-full">
                 <GraduationCap size={26} className="text-primary" />
@@ -20,7 +31,7 @@ const Education = () => {
                 <p className="text-lg lg:text-xl mt-2 text-gray-700">{edu.carrera}</p>
               </div>
             </div>
-          </div>
+          </motion.div>
         ))}
       </div>
     </div>
