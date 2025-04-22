89dc6eebfb1ee232814bf0de03e788febf0399c4
Arturo Esqueda <art270112@gmail.com>
2025-04-22 16:44:43 -0600

    Se inicia con al ultima parte que es el footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@
 const Footer = () => {
   return (
     <div className="border-t-2 border-t-gray-100">
-      <div className="my-10 text-center text-2xl md:text-4xl font-bold">Contacto</div>
+      <div className="my-10 text-2xl md:text-4xl font-bold">Contacto</div>
     </div>
   );
 }
