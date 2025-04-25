ef944fa2351d076c6199790853101ce26a71d7cf
Arturo Esqueda <art270112@gmail.com>
2025-04-25 17:34:44 -0600

    Se realiza ajuste en los textos en el footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -14,13 +14,13 @@ const Footer = () => {
     <div className="border-t-2 border-t-gray-100 md:p-2 m-auto lg:m-12">
       <div 
             className="flex-1 text-center m-10 md:text-left">
-            <h1 className="font-poppins font-bold text-2xl md:text-xl text-blue-500 mb-4">
+            <h1 className="font-poppins font-bold text-2xl md:text-2xl text-blue-500 mb-4">
               Luis Rodríguez
             </h1>
-            <h2 className="font-inter  font-bold text-lg md:text-xl text-bold mb-4">
+            <h2 className="font-inter  font-bold text-lg md:text-2xl text-bold mb-4">
               iOS Developer
             </h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-2 md:mt-2 text-sm text-gray-700">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-2 md:mt-2 text-xs text-gray-700">
               <a href="mailto:luuiz.rodriguez.lopez@gmail.com?Subject=Me interesa tu Perfil Profesional" 
                  className="flex items-center gap-2 justify-center md:justify-start hover:text-blue-500 hover:cursor-pointer">
                 <Mail size={24} className="text-primary" />
@@ -34,22 +34,21 @@ const Footer = () => {
                 <MapPin size={24} className="text-primary" />
                 <span className='text-lg'>Culiacan, Sinaloa</span>
               </div>
-              <div className="flex flex-col md:flex-row md:items-center md:justify-between w-full gap-4">
-                <div className="flex items-center justify-center gap-4 md:justify-start">
-                  <a href="https://github.com/luisrodriguezlopez" className="text-gray-700 hover:text-blue-500">
-                    <Github size={24} />
-                  </a>
-                  <a href="https://www.linkedin.com/in/luis-rodriguez-lopez-094118105/" className="text-gray-700 hover:text-blue-500">
-                    <Linkedin size={24} />
-                  </a>
-                </div>
-                <div className="flex justify-center md:justify-end ">
-                  <a href="#" className="flex hover:text-blue-500 text-base md:text-lg font-bold">
-                    Inicio <ArrowUp />
-                  </a>
-                </div>
-            </div>
-              
+                <div className="flex flex-col md:flex-row md:items-center md:justify-between w-full gap-4">
+                  <div className="flex items-center justify-center gap-4 md:justify-start">
+                    <a href="https://github.com/luisrodriguezlopez" className="text-gray-700 hover:text-blue-500">
+                      <Github size={24} />
+                    </a>
+                    <a href="https://www.linkedin.com/in/luis-rodriguez-lopez-094118105/" className="text-gray-700 hover:text-blue-500">
+                      <Linkedin size={24} />
+                    </a>
+                  </div>
+                  <div className="flex justify-center md:justify-end ">
+                    <a href="#" className="flex hover:text-blue-500 mt-4 text-base md:text-lg font-bold">
+                      Inicio <ArrowUp />
+                    </a>
+                  </div>
+              </div>
             </div>
           </div>
     </div>
